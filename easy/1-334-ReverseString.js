8d11c068504d2190099c9d521a543aa2184d2adc
Ning, Tao <ningtaous@gmail.com>
2016-05-12 23:21:01 -0400

    344. Reverse String

---
+++ easy/1-334-ReverseString.js
@@ -0,0 +1,29 @@
+// Write a function that takes a string as input and returns the string reversed.
+
+// Example:
+// Given s = 'hello', return 'olleh'.
+var should = require('chai').should();
+
+var reverseString = function(s) {
+    var i;
+    var res = '';
+
+    if (s === '') {
+        return s;
+    }
+
+    for (i = s.length - 1; i >= 0; i--) {
+        res += s.charAt(i);
+    }
+
+    return res;
+};
+
+describe('Test', function() {
+	it('should pass', function() {
+		reverseString('hello').should.equal('olleh');
+		reverseString('').should.equal('');
+		reverseString('o').should.equal('o');
+		reverseString('bood').should.equal('doob');
+	})
+})
