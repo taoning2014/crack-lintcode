cd2fbf60aea7ac85f0dcf1cd12fc4a6ed8ca6afa
Ning, Tao <ningtaous@gmail.com>
2016-05-19 18:01:47 -0400

    Add more solutions to 94

--- lecture-3-dfs-in-bs/2-94-BinaryTreeInorderTraversal.js
+++ lecture-3-dfs-in-bs/2-94-BinaryTreeInorderTraversal.js
@@ -44,24 +44,52 @@ function TreeNode(val) {
 // };
 
 // Solution 2
+// var inorderTraversal = function(root) {
+//   var inOrder = [];
+//   var leftArray;
+//   var rightArray;
+
+//   if (!root) {
+//     return inOrder;
+//   }
+
+//   leftArray = inorderTraversal(root.left);
+//   rightArray = inorderTraversal(root.right);
+
+//   inOrder = inOrder.concat(leftArray);
+//   inOrder.push(root.val);
+//   inOrder = inOrder.concat(rightArray);
+
+//   return inOrder;
+// };
+
+// Solution 3 no recursive using stack
 var inorderTraversal = function(root) {
   var inOrder = [];
-  var leftArray;
-  var rightArray;
+  var stack = [];
 
   if (!root) {
     return inOrder;
   }
 
-  leftArray = inorderTraversal(root.left);
-  rightArray = inorderTraversal(root.right);
-
-  inOrder = inOrder.concat(leftArray);
-  inOrder.push(root.val);
-  inOrder = inOrder.concat(rightArray);
+  stack.push(root);
+  while (stack.length) {
+    var temp = stack.pop();
+    if (temp instanceof TreeNode) {
+      if (temp.right) {
+        stack.push(temp.right);
+      }
+      stack.push(temp.val);
+      if (temp.left) {
+        stack.push(temp.left);
+      }
+    } else {
+      inOrder.push(temp);
+    }
+  }
 
   return inOrder;
-};
+}
 
 describe('Test', function() {
   var n0;
@@ -72,20 +100,25 @@ describe('Test', function() {
 
   before(function() {
     n0 = new TreeNode(0);
-    n1 = new TreeNode(1);
-    n2 = new TreeNode(2);
-    n3 = new TreeNode(3);
-    n4 = new TreeNode(4);
-    n0.left = n1;
-    n0.right = n2;
-    n1.left = n3;
-    n1.right = n4;
-  });
+  //   n1 = new TreeNode(1);
+  //   n2 = new TreeNode(2);
+  //   n3 = new TreeNode(3);
+  //   n4 = new TreeNode(4);
+  //   n0.left = n1;
+  //   n0.right = n2;
+  //   n1.left = n3;
+  //   n1.right = n4;
+  // });
 
   it('Should Pass', function() {
     var resArray = inorderTraversal(n0);
-    resArray.length.should.equal(5);
-    resArray[0].should.equal(3);
-    resArray[4].should.equal(2);
+    console.log(resArray);
   });
+
+  // it('Should Pass', function() {
+  //   var resArray = inorderTraversal(n0);
+  //   resArray.length.should.equal(5);
+  //   resArray[0].should.equal(3);
+  //   resArray[4].should.equal(2);
+  // });
 });
