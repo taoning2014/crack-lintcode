5326bf855b31285e64814a69fc7336aba01ff73b
Ning, Tao <ningtaous@gmail.com>
2016-06-01 22:24:15 -0400

    new solution 303

--- easy-61-80/74-303-RangeSumQueryImmutable.js
+++ easy-61-80/74-303-RangeSumQueryImmutable.js
@@ -58,25 +58,52 @@ require('chai').should();
  * @constructor
  * @param {number[]} nums
  */
+// var NumArray = function(nums) {
+//   var i;
+//   var j;
+
+//   this.record = [];
+
+//   for (i = 0; i < nums.length; i++) {
+//     this.record[i] = [];
+//     this.record[i][i] = nums[i];
+//   }
+
+//   for (i = 0; i < nums.length; i++) {
+//     for (j = i + 1; j < nums.length; j++) {
+//       this.record[i][j] = this.record[i][j-1] + nums[j];
+//     }
+//   }
+
+//   console.log('Construct matrix: ');
+//   console.log(this.record);
+// };
+
+/**
+ * @param {number} i
+ * @param {number} j
+ * @return {number}
+ */
+// NumArray.prototype.sumRange = function(i, j) {
+//   return this.record[i][j];
+// };
+
+
+// Solution 3. From: https://leetcode.com/discuss/98846/java-concise-solution-without-if
+/**
+ * @constructor
+ * @param {number[]} nums
+ */
 var NumArray = function(nums) {
   var i;
-  var j;
 
   this.record = [];
+  this.record[0] = nums[0];
 
-  for (i = 0; i < nums.length; i++) {
-    this.record[i] = [];
-    this.record[i][i] = nums[i];
+  for (i = 1; i < nums.length; i++) {
+    this.record[i] = this.record[i - 1] + nums[i];
   }
 
-  for (i = 0; i < nums.length; i++) {
-    for (j = i + 1; j < nums.length; j++) {
-      this.record[i][j] = this.record[i][j-1] + nums[j];
-    }
-  }
-
-  console.log('Construct matrix: ');
-  console.log(this.record);
 };
 
 /**
@@ -85,9 +112,12 @@ var NumArray = function(nums) {
  * @return {number}
  */
 NumArray.prototype.sumRange = function(i, j) {
-  return this.record[i][j];
+  if (i === 0) {
+    return this.record[j];
+  } else {
+    return this.record[j] - this.record[i - 1];
+  }
 };
-
 /**
  * Your NumArray object will be instantiated and called as such:
  * var numArray = new NumArray(nums);
@@ -100,5 +130,6 @@ describe('Test', function() {
     var numArray = new NumArray(nums);
     console.log(numArray.sumRange(0, 1));
     console.log(numArray.sumRange(0, 2));
+    console.log(numArray.sumRange(1, 2));
   });
 });
