f5145336a4a21678c8e480e7fbe06c5623f2eb8d
Ning, Tao <ningtaous@gmail.com>
2016-06-01 22:23:26 -0400

    1 new solution

--- advanced-lecture-1/1-1-TwoSum.js
+++ advanced-lecture-1/1-1-TwoSum.js
@@ -13,10 +13,11 @@
 // =====
 // Note:
 // 1, Add more test case: 0, negative...
-
+// 2, Awesome o(n) solution: http://www.jiuzhang.com/solutions/two-sum/
 'use strict';
 require('chai').should();
 
+// Solution 1.
 function binarySearch(nums, target) {
   var l = 0;
   var r = nums.length - 1;
@@ -41,53 +42,73 @@ function binarySearch(nums, target) {
  * @param {number} target
  * @return {number[]}
  */
+// var twoSum = function(nums, target) {
+//   var i;
+//   var index;
+//   var sorted;
+//   var p1;
+//   var p2;
+//   var startFrom = 0;
+
+//   if (!nums || nums.length < 2) {
+//     return [-1, -1];
+//   }
+
+//   // BUG: Need to keep refer to the origin array, just copy refer won't work
+//   // sorted = nums
+//   sorted = nums.slice();
+
+//   sorted.sort(function(p, q) {
+//     return parseInt(p, 10) - parseInt(q, 10);
+//   });
+
+//   for (i = 0; i < sorted.length; i++) {
+//     // BUG: Originally I want to jump the rest if the current elem is larger that target
+//     //  I didn't think of negative numbers
+//     // for (i = 0; i < sorted.length && sorted[i] <= target; i++) {
+//     index = binarySearch(sorted, target - sorted[i]);
+//     if (index !== -1) {
+//       p1 = nums.indexOf(sorted[i]);
+//       // BUG: Can't pass [0, 4, 3, 0], 0. Since both of them will return first element position
+//       if (sorted[i] === sorted[index]) {
+//         startFrom = p1 + 1;
+//       }
+
+//       p2 = nums.indexOf(sorted[index], startFrom);
+//       return (p1 < p2 ? [p1, p2] : [p2, p1])
+//     }
+//   }
+
+//   return [-1, -1];
+// };
+
+// Solution 2.
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
 var twoSum = function(nums, target) {
+  var map = {};
   var i;
-  var index;
-  var sorted;
-  var p1;
-  var p2;
-  var startFrom = 0;
 
-  if (!nums || nums.length < 2) {
-    return [-1, -1];
-  }
-
-  // BUG: Need to keep refer to the origin array, just copy refer won't work
-  // sorted = nums
-  sorted = nums.slice();
-
-  sorted.sort(function(p, q) {
-    return parseInt(p, 10) - parseInt(q, 10);
-  });
-
-  for (i = 0; i < sorted.length; i++) {
-    // BUG: Originally I want to jump the rest if the current elem is larger that target
-    //  I didn't think of negative numbers
-    // for (i = 0; i < sorted.length && sorted[i] <= target; i++) {
-    index = binarySearch(sorted, target - sorted[i]);
-    if (index !== -1) {
-      p1 = nums.indexOf(sorted[i]);
-      // BUG: Can't pass [0, 4, 3, 0], 0. Since both of them will return first element position
-      if (sorted[i] === sorted[index]) {
-        startFrom = p1 + 1;
-      }
-
-      p2 = nums.indexOf(sorted[index], startFrom);
-      return (p1 < p2 ? [p1, p2] : [p2, p1])
+  for (i = 0; i < nums.length; i++) {
+    // BUG: can't do if (map[nums[i]]) since when the key is 0, this will faulse
+    if (map[nums[i]] !== undefined) {
+      return [map[nums[i]] + 1, i + 1];
     }
+
+    map[target - nums[i]] = i;
   }
 
   return [-1, -1];
-};
-
-
+}
 describe('Test', function() {
   it('Should pass', function() {
     // console.log(twoSum([2, 7, 11, 15], 13));
     // console.log(twoSum([2, 7, 11, 15], 22));
     // console.log(twoSum([3, 2, 4], 6));
-    console.log(twoSum([0, 4, 3, 0], 3));
+    console.log(twoSum([0, 4, 3, 1], 3));
     console.log(twoSum([0, 4, 3, 0], 0));
     console.log(twoSum([-1, -2, -3, -4, -5], -8));
     // Won't happen. 'You may assume that each input would have exactly one solution.'
