8a9341ea825554222f71dcc17a95ae4800b09d8a
Tao Ning <ningtaous@gmail.com>
2016-06-15 22:25:04 -0400

    204

---
+++ easy-61-90/83-204-CountPrimes.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Count the number of prime numbers less than a non-negative number, n.
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+
+};
