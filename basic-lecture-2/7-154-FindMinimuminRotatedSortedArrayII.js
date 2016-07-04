3afcbeaa2288955d5f8a032e276d752f657d35f8
Tao Ning <ningtaous@gmail.com>
2016-07-03 22:57:28 -0400

    154

--- basic-lecture-2/7-154-FindMinimuminRotatedSortedArrayII.js
+++ basic-lecture-2/7-154-FindMinimuminRotatedSortedArrayII.js
@@ -14,27 +14,74 @@
 'use strict';
 require('chai').should();
 
+// var _findMin = function(nums) {
+//   var l;
+//   var r;
+//   var mid;
+
+//   if (!nums || !nums.length) {
+//     return;
+//   }
+
+//   l = 0;
+//   r = nums.length - 1;
+//   while (l <= r) {
+//     mid = l + Math.floor((r - l) / 2);
+//     if (nums[nums.length - 1] < nums[mid]) {
+//       l = mid + 1;
+//     } else {
+//       r = mid - 1;
+//     }
+//   }
+
+//   return nums[l];
+// };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// var findMin = function(nums) {
+//   var i;
+//   if (!nums || !nums.length) {
+//     return NaN;
+//   }
+
+//   // process first few elements which equals to last
+//   for (i = 0; i < nums.length - 1; i++) {
+//     if (nums[i] !== nums[nums.length - 1]) {
+//       break;
+//     }
+//   }
+
+//   nums = nums.slice(i);
+//   return _findMin(nums);
+// };
+
+// Solution 2. Refer: https://leetcode.com/discuss/98764/simple-c-binary-search-solution-with-explanation
+
 var _findMin = function(nums) {
-  var l;
-  var r;
+  var l = 0;
+  var r = nums.length - 1;;
   var mid;
 
-  if (!nums || !nums.length) {
-    return;
-  }
-
-  l = 0;
-  r = nums.length - 1;
   while (l <= r) {
     mid = l + Math.floor((r - l) / 2);
-    if (nums[nums.length - 1] < nums[mid]) {
+    if (nums[mid] === nums[nums.length - 1]) {
+      // BUG: Can't use r = mid - 1 and l = mid + 1. 因为在rotate的那个地方不能直接jump过去，而需要一个个遍历
+      if (nums[l] < nums[nums.length - 1]) {
+        r--;
+      } else {
+        l++;
+      }
+    } else if (nums[nums.length - 1] < nums[mid]) {
       l = mid + 1;
     } else {
       r = mid - 1;
     }
   }
-
-  return nums[l];
+  // BUG: directly return nums[l] can pass [1,1,1,1]
+  return (l === nums.length ? nums[l - 1] : nums[l]);
 };
 
 /**
@@ -43,29 +90,23 @@ var _findMin = function(nums) {
  */
 var findMin = function(nums) {
   var i;
+
   if (!nums || !nums.length) {
     return NaN;
   }
 
-  // process first few elements which equals to last
-  for (i = 0; i < nums.length - 1; i++) {
-    if (nums[i] !== nums[nums.length - 1]) {
-      break;
-    }
-  }
-
-  nums = nums.slice(i);
   return _findMin(nums);
 };
 
 describe('Test', function() {
   it('Should pass', function() {
     console.log(findMin([]));
+    console.log(findMin([10, 1, 10, 10, 10]))
     console.log(findMin([1]));
     console.log(findMin([1, 2]));
     console.log(findMin([3, 1, 3]));
     console.log(findMin([3, 1, 3, 3]));
-    // console.log(findMin([4, 5, 6, 7, 8, 1]));
-    // console.log(findMin([4, 5, 6, 7, 8]));
+    console.log(findMin([4, 5, 6, 7, 8, 1]));
+    console.log(findMin([4, 5, 6, 7, 8]));
  })
 })
