2eec3c7876b5e6f47f9889668ad5d8f58b58da82
Tao Ning <ningtaous@gmail.com>
2016-07-26 23:01:20 -0400

    add new solution

--- easy-61-90/71-14-LongestCommonPrefix.js
+++ easy-61-90/71-14-LongestCommonPrefix.js
@@ -38,6 +38,28 @@ var longestCommonPrefix = function(strs) {
   return shortestStr;
 };
 
+// Solution 2, refer: https://discuss.leetcode.com/topic/27913/sorted-the-array-java-solution-2-ms
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  if (!Array.isArray(strs) || strs.length === 0) {
+    return '';
+  }
+
+  strs.sort();
+  const m = strs[0].length;
+  const n = strs[strs.length - 1].length;
+  for (let i = 0; i < m && i < n; i++) {
+    if (strs[0][i] !== strs[strs.length - 1][i]) {
+      return strs[0].substr(0, i);
+    }
+  }
+
+  return m < n ? strs[0] : strs[strs.length - 1];
+}
+
 describe('Test', function () {
   it('Should pass', function () {
     console.log(longestCommonPrefix(['hello', 'heis', 'hell']));
