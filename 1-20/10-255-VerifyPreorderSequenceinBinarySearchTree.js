e32680e3e20347fcaa4153afe1ca4aed5d14a274
Tao Ning <ningtaous@gmail.com>
2016-07-29 18:37:44 -0400

    255

---
+++ 1-20/10-255-VerifyPreorderSequenceinBinarySearchTree.js
@@ -0,0 +1,35 @@
+// Given an array of numbers, verify whether it is the correct preorder traversal
+// sequence of a binary search tree.
+
+// You may assume each number in the sequence is unique.
+
+// Follow up:
+// Could you do it using only constant space complexity?
+
+// Refer: https://discuss.leetcode.com/topic/21217/java-o-n-and-o-1-extra-space
+
+/**
+ * @param {number[]} preorder
+ * @return {boolean}
+ */
+var verifyPreorder = function(preorder) {
+  if (!Array.isArray(preorder) || preorder.length === 0) {
+    return false;
+  }
+
+  const stack = [];
+  let curMin = Number.NEGATIVE_INFINITY;
+  for (let i = 0; i < preorder.length; i++) {
+    if (preorder[i] < curMin) {
+      return false;
+    }
+
+    while (stack.length !== 0 && preorder[i] > stack[stack.length - 1]) {
+      curMin = stack.pop();
+    }
+
+    stack.push(preorder[i]);
+  }
+
+  return true;
+};
