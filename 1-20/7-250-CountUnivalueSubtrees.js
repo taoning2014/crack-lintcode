e44b3ef851dfb837c912a4da325da70c486f64de
Tao Ning <ningtaous@gmail.com>
2016-07-29 18:41:08 -0400

    250

---
+++ 1-20/7-250-CountUnivalueSubtrees.js
@@ -0,0 +1,22 @@
+// Given a binary tree, count the number of uni-value subtrees.
+
+// A Uni-value subtree means all nodes of the subtree have the same value.
+
+// For example:
+// Given binary tree,
+//               5
+//              / \
+//             1   5
+//            / \   \
+//           5   5   5
+// return 4.
+// [5,5,5,5,5,null,5]
+var count;
+
+// Definition for a binary tree node.
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+
