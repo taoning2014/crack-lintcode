32d9d804b58a8a7afffa4e045d6945f05d1ec144
Tao Ning <ningtaous@gmail.com>
2016-07-31 11:20:09 -0400

    time exceed

--- 21-40/25-247-StrobogrammaticNumberII.js
+++ 21-40/25-247-StrobogrammaticNumberII.js
@@ -7,12 +7,79 @@
 // For example,
 // Given n = 2, return ["11","69","88","96"].
 
+// function permutation(nums, cur, len, result) {
+//   if (cur.length === len) {
+//     if (len === 1 || cur[len - 1] !== '0') {
+//       result.push(cur);
+//     }
+
+//     return;
+//   }
+
+//   for (let i = 0; i < nums.length; i++) {
+//     const copyCur = cur.slice();
+//     copyCur.push(nums[i]);
+//     permutation(nums, copyCur, len, result);
+//   }
+// }
+
+// function isStrobogrammatic(x, map) {
+//   let len = x.length;
+
+//   // if odd, mid should be '6' or '9'
+//   if (len % 2 === 1 && ['6', '9'].indexOf(x[Math.floor(len / 2)]) !== -1) {
+//     return false;
+//   }
+
+//   for (let i = 0; i < Math.floor(len / 2); i++) {
+//     if (map[x[i]] !== x[len - 1 - i]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+// /**
+//  * @param {number} n
+//  * @return {string[]}
+//  */
+// var findStrobogrammatic = function(n) {
+//   if (!Number.isInteger(n) || n < 1) {
+//     return [];
+//   }
+
+//   // create permutation
+//   const result = [];
+//   permutation(['0', '1', '6', '8', '9'], [], n, result);
+
+//   // filter out result
+//   const map = {
+//     '0': '0',
+//     '1': '1',
+//     '8': '8',
+//     '6': '9',
+//     '9': '6'
+//   };
+//   return result
+//     .filter(x => isStrobogrammatic(x, map))
+//     .map(x => x.join(''));
+// };
+
+var map = {
+  '0': '0',
+  '1': '1',
+  '8': '8',
+  '6': '9',
+  '9': '6'
+};
+
 function permutation(nums, cur, len, result) {
   if (cur.length === len) {
-    if (len === 1 || cur[len - 1] !== '0') {
+    cur = cur.join('');
+    if (isStrobogrammatic(cur)) {
       result.push(cur);
     }
-
     return;
   }
 
@@ -23,8 +90,13 @@ function permutation(nums, cur, len, result) {
   }
 }
 
-function isStrobogrammatic(x, map) {
+function isStrobogrammatic(x) {
   let len = x.length;
+
+  // last should not be 0
+  if (len > 1 && x[len - 1] === '0') {
+    return false;
+  }
 
   // if odd, mid should be '6' or '9'
   if (len % 2 === 1 && ['6', '9'].indexOf(x[Math.floor(len / 2)]) !== -1) {
@@ -53,17 +125,7 @@ var findStrobogrammatic = function(n) {
   const result = [];
   permutation(['0', '1', '6', '8', '9'], [], n, result);
 
-  // filter out result
-  const map = {
-    '0': '0',
-    '1': '1',
-    '8': '8',
-    '6': '9',
-    '9': '6'
-  };
-  return result
-    .filter(x => isStrobogrammatic(x, map))
-    .map(x => x.join(''));
+  return result;
 };
 
 console.log(findStrobogrammatic(1));
