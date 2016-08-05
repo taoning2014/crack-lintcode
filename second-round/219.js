bbd05fcbd57b6c9c1b8d8cb94c94603f8318eeb0
Tao Ning <ningtaous@gmail.com>
2016-08-04 22:35:44 -0400

    219

---
+++ second-round/219.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// ========================================================================
+// Time:   8min
+// Submit: 2 can't call exchange
+// ========================================================================
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+  if (!Array.isArray(nums) || !Number.isInteger(k) || k < 1) {
+    return false;
+  }
+
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      if (i - map.get(nums[i]) <= k) {
+        return true;
+      } else {
+        map.set(nums[i], i);
+      }
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+
+  return false;
+};
