4e8e811739b9957e72df8026392b52a8c256fb4e
Tao Ning <ningtaous@gmail.com>
2016-08-08 23:36:28 -0400

    156

---
+++ second-temp/156.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// ========================================================================
+// Time:   5min
+// Submit: 1
+// ========================================================================
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var upsideDownBinaryTree = function(root) {
+  if (!root || !root.left) {
+    return root;
+  }
+
+  const newRoot = upsideDownBinaryTree(root.left);
+  root.left.left = root.right;
+  root.left.right = root;
+  root.left = null;
+  root.right = null;
+  return newRoot;
+};
