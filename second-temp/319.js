7ca8a20e6f79bb38d1111eb494fc57289b260585
Tao Ning <ningtaous@gmail.com>
2016-08-08 23:37:20 -0400

    319

---
+++ second-temp/319.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// ========================================================================
+// Time:   5min
+// Submit: 1
+// ========================================================================
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var bulbSwitch = function(n) {
+  return Math.floor(Math.sqrt(n));
+};
